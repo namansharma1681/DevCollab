834341558a29775a8eb7254e61edcc9422f62359
Naman Sharma <namansharmavivoy30@gmail.com>
2026-05-13 23:31:17 +0530

    made signup page and connected it with react routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import LandingPage from './pages/LandingPage'
 import {Routes, Route} from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage/>}/>
       <Route path="/login" element={<LoginPage/>} />
+      <Route path="/signup" element={<SignupPage/>}/>
     </Routes>
   )
 }
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,14 +1,23 @@
-import { Link }  from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function LoginPage() {
+function SignupPage() {
   return (
     <div className="min-h-screen bg-gray-950 text-white flex items-center justify-center">
       <div className="bg-gray-900 p-10 rounded-xl border border-gray-800 w-full max-w-md">
-        
-        <h2 className="text-3xl font-bold mb-2 text-center">Welcome Back</h2>
-        <p className="text-gray-400 text-center mb-8">Login to your account</p>
+
+        <h2 className="text-3xl font-bold mb-2 text-center">Create Account</h2>
+        <p className="text-gray-400 text-center mb-8">Join DevCollab today</p>
 
         <div className="flex flex-col gap-4">
+          <div>
+            <label className="text-sm text-gray-400 mb-1 block">Full Name</label>
+            <input
+              type="text"
+              placeholder="Naman Sharma"
+              className="w-full bg-gray-800 border border-gray-700 rounded-lg px-4 py-3 text-white focus:outline-none focus:border-purple-500"
+            />
+          </div>
+
           <div>
             <label className="text-sm text-gray-400 mb-1 block">Email</label>
             <input
@@ -28,13 +37,13 @@ function LoginPage() {
           </div>
 
           <button className="w-full bg-purple-600 py-3 rounded-lg font-bold hover:bg-purple-700 cursor-pointer mt-2">
-            Login
+            Create Account
           </button>
         </div>
 
         <p className="text-center text-gray-400 mt-6">
-          Don't have an account?{' '}
-          <Link to = "/signup"><span className="text-purple-500 cursor-pointer hover:underline">Sign Up</span></Link>
+          Already have an account?{' '}
+          <Link to="/login" className="text-purple-500 hover:underline">Login</Link>
         </p>
 
       </div>
@@ -42,4 +51,4 @@ function LoginPage() {
   )
 }
 
-export default LoginPage
+export default SignupPage
